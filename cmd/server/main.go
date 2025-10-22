a132e5643e83775f89e4abf65f3e3a4f538fa665
nandowastaken <fernandojorge.cavalcantegomes@gmail.com>
2025-10-22 09:12:27 -0300

    feat: publishJson function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Println("Something went wrong on connection.")
+		return
+	}
+
+	defer connection.Close()
+
+	fmt.Println("Connection went successfully!")
+
+
+
+	if err != nil {
+		fmt.Println("Something went wrong on creating a channel.");
+		return;
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	sig := <-signalChan
+
+	fmt.Printf("\nReceived signal: %s\n", sig)
 }
